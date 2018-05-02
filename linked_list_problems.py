e759ab2d5fdf2b8bcd87c3cea24e4fbd1bfd3faa
DanPetruso <31451444+DanPetruso@users.noreply.github.com>
2018-05-02 16:24:53 -0400

    nth to last node

--- linked_list_problems.py
+++ linked_list_problems.py
@@ -1,16 +1,16 @@
-#--------------CYCLE-CHECK--------------------
-#Given a singly linked list, write a function which takes in the first node in a singly 
-#linked list and returns a boolean indicating if the linked list contains a "cycle".
-#A cycle is when a node's next point actually points back to a previous node in the list.
-# This is also sometimes known as a circularly linked list.
-#You've been given the Linked List Node class code:
-
 class Node(object):
     
     def __init__(self,value):
         
         self.value = value
         self.nextnode = None
+
+#--------------CYCLE-CHECK--------------------
+#Given a singly linked list, write a function which takes in the first node in a singly 
+#linked list and returns a boolean indicating if the linked list contains a "cycle".
+#A cycle is when a node's next point actually points back to a previous node in the list.
+# This is also sometimes known as a circularly linked list.
+#You've been given the Linked List Node class code:
 
 def cycle_check(node):
     
@@ -25,3 +25,18 @@ def cycle_check(node):
         if n1 == n2:
             return True
     return False
+
+
+##------------------Nth-TO-LAST-NODE-----------------------
+#Write a function that takes a head node and an integer value n and then returns 
+#the nth to last node in the linked list.
+    
+def nth_to_last_node(n, head):
+    l = [head]
+    curr = head
+    while curr.nextnode != None:
+        l.append(curr)
+    return l[len(l) - n]
+
+
+
