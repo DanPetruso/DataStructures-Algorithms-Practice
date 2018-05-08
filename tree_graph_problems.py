0c2d4756a6157a7b5bcfb7f6a500d4a152c11114
DanPetruso <31451444+DanPetruso@users.noreply.github.com>
2018-05-07 21:54:20 -0400

    validate bst

---
+++ tree_graph_problems.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self, val=None):
+        self.left = None
+        self.right = None
+        self.val = val 
+
+
+#------------------------VALIDATE-BINARY-SEARCH-TREE----------------
+def check(node, minval, maxval):
+    if node == None:
+        return True
+    if node.val > maxval or node.val < minval:
+        return False
+    return check(node.left, minval, node.val) and check(node.right, node.val, maxval)
+    
