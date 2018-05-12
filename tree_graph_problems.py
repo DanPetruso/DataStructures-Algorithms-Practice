6b612fd47d4c22dba05c73c87b1fc63902b051ab
DanPetruso <31451444+DanPetruso@users.noreply.github.com>
2018-05-12 13:32:05 -0400

    inorder

--- tree_graph_problems.py
+++ tree_graph_problems.py
@@ -12,4 +12,12 @@ def check(node, minval, maxval):
     if node.val > maxval or node.val < minval:
         return False
     return check(node.left, minval, node.val) and check(node.right, node.val, maxval)
-    
+
+
+#-----------------------INORDER-TREE-PRINT------------------------
+def inorder(tree):
+    while tree != None:
+        inorder(tree.left)
+        print(tree.val)
+        inorder(tree.right)
+        
